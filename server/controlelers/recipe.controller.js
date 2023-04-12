61c04e107fa09a67ce63f3003e3175a8d332da18
Lopez-CL <cllopez511@gmail.com>
2023-04-12 15:04:00 -0400

    built v.1 of controllers

--- server/controlelers/recipe.controller.js
+++ server/controlelers/recipe.controller.js
@@ -1,3 +1,51 @@
 
 const Recipe = require('../models/recipe.model');
 
+
+const createRecipe = (req, res) => {
+    Recipe.create(req.body)
+        .then(newRecipe => res.json(newRecipe))
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+}
+
+const getAllRecipes = (req, res) => {
+    Recipe.find()
+        .then(recipes => res.json(recipes))
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+}
+const getRecipeById = (req, res) => {
+    Recipe.findOne({_id: req.params._id})
+        .then(recipe => res.json(recipe))
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+}
+
+const updateRecipe = (req, res) => {
+    Recipe.findOneAndUpdate({_id: req.params._id}, req.body, {
+        runValidators: true,
+        new: true,
+    })
+        .then(updatedRecipe =>{
+            res.json(updateRecipe)
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+}
+const deleteRecipe = (req, res) => {
+    Recipe.deleteOne({_id: req.params._id})
+        .then(deletedRecipe => res.json(deletedRecipe))
+        .catch(err =>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+}
