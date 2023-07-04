b0e738dec74f4d6180f46c95ffc984fd639cbc3c
Lopez-CL <cllopez511@gmail.com>
2023-07-04 11:23:15 -0400

    finished draft 1 of navbar

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,14 +1,10 @@
-import Typography  from "@material-ui/core/Typography";
-import { useTheme } from "@mui/material/styles";
-import { Button } from "@mui/material";
+// // import Typography  from "@material-ui/core/Typography";
+// import { useTheme } from "@mui/material/styles";
 
 const Dashboard = () =>{
-    const theme = useTheme();
+    // const theme = useTheme();
     return(
         <>
-            <Button variant="outlined" color="primary">
-                Submit
-            </Button>
         </>
     )
 }
