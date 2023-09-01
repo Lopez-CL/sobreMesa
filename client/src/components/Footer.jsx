fc3e24eaee38eb380c9eb3c3c3a72e715410f66a
Lopez-CL <cllopez511@gmail.com>
2023-09-01 13:53:46 -0400

    styled recipe container and addressed expansion issue

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () =>{
-
+    return(
+        <>
+            <div>
+                <p>Placeholder</p>
+            </div>
+        </>
+    )
 };
 
 export default Footer;
