c116947aaa4732d8aeefb2bebacb0b3ecb32e79b
Lopez-CL <cllopez511@gmail.com>
2023-11-21 18:08:49 -0500

    added check box strike through feature

--- client/src/components/InstList.jsx
+++ client/src/components/InstList.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup  from "@mui/material/FormGroup";
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { useTheme } from "@mui/material/styles";
 const InstrList = (props)=>{
     const {instructions} = props
+    const [instListNums, setInstListNums] = useState([]);
+    const handleItemNum = itemNum =>{
+        setInstListNums(currentInstListNums =>{
+            if(!currentInstListNums.includes(itemNum)){
+                return [...currentInstListNums, itemNum]
+            }
+            else{
+                return currentInstListNums.filter(num => num !== itemNum)}
+        })}
     return(
         <FormGroup>
             {instructions.map((item,index)=>(
-                <FormControlLabel key={index} control={<Checkbox/>} label={item}/>
+                <FormControlLabel className={instListNums.includes(index)?'completed-list-item':''} key={index} 
+                    control={<Checkbox
+                    checked={instListNums.includes(index)}
+                    onChange={ e => handleItemNum(index)}
+                />} 
+                label={item}/>
             ))}
         </FormGroup>
     )
